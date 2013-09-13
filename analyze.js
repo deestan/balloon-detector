932ef8c552d8c8b39f980f0ee76974fec5510b36
Helge Skogly Holm <helge.holm@gmail.com>
2013-09-13 14:52:52 +0200

    More sophisticated detection, and do not return NaN.  More test data.

--- analyze.js
+++ analyze.js
@@ -1,11 +1,15 @@
 var PNG = require('png-js');
 var fs = require('fs');
+var rgb2hsl = require('color-convert').rgb2hsl;
 
 var width = 640;
 var rowlen = width * 4;
+var hue = 214;
+var hueMin = 214 - 5;
+var hueMax = 214 + 5;
 
-var minMatches = 8000;
-var maxMatches = 82000;
+var minMatches = 1500;
+var maxMatches = 25000;
 
 function isMatch(r, g, b, a) {
   var z = r + g + b;
@@ -24,17 +28,23 @@ module.exports = function detectBalloon(pngData, next) {
     for (var i=0; i < data.length - 4; i += 4) {
       var x = Math.floor((i % rowlen) / 4);
       var y = Math.floor(i / rowlen);
-      if (isMatch(data[i], data[i+1], data[i+2], data[i+3])) {
+
+      var hsl = rgb2hsl(data[i], data[i+1], data[i+2]);
+      if (hsl[0] > hueMin && hsl[0] < hueMax && hsl[1] > 50 && hsl[2] > 20) {
         x_sum += x;
         y_sum += y;
         matches += 1;
       }
     }
+    
+    if (!matches)
+      return next(null, { balloon: false, x: 0, y: 0 });
+    
     var x_avg = x_sum / matches;
     var y_avg = y_sum / matches;
     var x_avg_rel = 1 - (2 * x_avg / width);
     var y_avg_rel = 1 - (2 * y_avg / height);
-    
+
     next(null, {
       balloon: (matches > minMatches && matches < maxMatches),
       x: -x_avg_rel,
